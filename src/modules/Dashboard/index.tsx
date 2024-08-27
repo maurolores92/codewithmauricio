aa4512f1c2bb4bf4c0e13310eb5a4bdaf9a5bdc7
Mauricio <maurolores1992@gmail.com>
2024-08-26 21:48:40 -0300

    dashboard

--- src/modules/Dashboard/index.tsx
+++ src/modules/Dashboard/index.tsx
@@ -21,10 +21,6 @@ const AdminDashboard: React.FC = () => {
 
       {/* Main Sidebar */}
       <aside className="main-sidebar sidebar-dark-primary elevation-4">
-        <a href="#" className="brand-link">
-          <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
-          <span className="brand-text font-weight-light">AdminLTE 3</span>
-        </a>
         <div className="sidebar">
           <nav className="mt-2">
             <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
